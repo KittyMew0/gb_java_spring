a0810cfb86820d67840f427dbd5f5fd74e026532
KittyMew0 <90781550+KittyMew0@users.noreply.github.com>
2024-10-19 19:31:20 +0300

    Add files via upload

---
+++ java_maven/src/java_maven/Main.java
@@ -0,0 +1,10 @@
+package java_maven;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this project was built with maven");
+	}
+
+}
